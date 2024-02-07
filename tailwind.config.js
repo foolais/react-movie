68140d40cdad6fb3ac9a8cb578b430fef9b18bbd
wahyu.esya17 <wahyu.esya17@gmail.com>
2024-02-07 21:18:34 +0700

    finishing setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "rgb(245,197,24)",
+        secondary: "rgba(18,18,18)",
+      },
+    },
+  },
+  plugins: [],
+};
