53da63182c2246a1ed8386d10cee5ba199bd93bd
wahyu.esya17 <wahyu.esya17@gmail.com>
2024-02-07 23:20:04 +0700

    add tablet, desktop view navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,26 +13,37 @@ const Navbar = () => {
   const inputRef = useRef(null);
   const isMobile = useMediaQuery({ maxWidth: 768 });
 
-  const handleClickSearch = () => {
-    setIsSearching((prev) => !prev);
-  };
-
   useEffect(() => {
     if (isSearching && inputRef.current) inputRef.current.focus();
   }, [isSearching]);
 
   return (
-    <div className="bg-secondary flex h-16 w-screen items-center justify-between p-4">
+    <div className="bg-secondary flex h-16 w-screen items-center justify-between p-5">
       <div className="flex items-center gap-4">
         <div
           onClick={() => setIsOpenSideNav((prev) => !prev)}
-          className={twMerge("text-white cursor-pointer text-3xl", "md:hidden")}
+          className={twMerge(
+            "text-white cursor-pointer text-3xl",
+            "md:hidden ",
+          )}
         >
           <HiMenu className="hover:text-primary" />
         </div>
         <div className="w-max cursor-pointer rounded-md bg-primary p-1 text-2xl font-extrabold hover:opacity-80">
           IMDb
         </div>
+        {!isMobile && (
+          <div
+            className={twMerge(
+              "text-white ml-2 flex items-center justify-center gap-3 text-xl font-semibold",
+              "lg:gap-4 lg:tracking-wider",
+            )}
+          >
+            <span className="hover:text-primary">Popular</span>
+            <span className="hover:text-primary">Top Rated</span>
+            <span className="hover:text-primary">Upcoming</span>
+          </div>
+        )}
       </div>
       {/* searching bar for mobile */}
       {isMobile && isSearching && (
@@ -40,7 +51,7 @@ const Navbar = () => {
           <input
             type="text"
             onChange={(e) => setSearchData(e.target.value)}
-            placeholder="Search Movie here...."
+            placeholder="Search Movie Here...."
             className={twMerge(
               "bg-secondary text-white h-16 w-full p-3 ",
               "placeholder:text-textPrimary pr-10 placeholder:font-bold focus:outline-none",
@@ -57,26 +68,41 @@ const Navbar = () => {
           </div>
         </div>
       )}
-      <div className="flex items-center gap-4">
+      {/* END searching bar for mobile */}
+      <div
+        className={twMerge("flex items-center gap-4", "md:w-[40%] lg:w-1/2")}
+      >
         {isMobile ? (
           // open from mobile size
           <div
             className="text-white cursor-pointer text-xl"
-            onClick={() => handleClickSearch()}
+            onClick={() => setIsSearching((prev) => !prev)}
           >
             <HiOutlineSearch className="hover:text-primary" />
           </div>
         ) : (
           // open from above tablet size
-          <div className="bg- flex items-center">
-            <input type="text" />
-            <div className="text-white text-xl">
-              <HiOutlineSearch
-                className="hover:text-primary"
-                onClick={() => setIsSearching((prev) => !prev)}
+          <>
+            <div className="bg-white relative flex w-full items-center rounded-md p-2">
+              <input
+                type="text"
+                placeholder="Search Movie Here..."
+                onChange={(e) => setSearchData(e.target.value)}
+                className={twMerge(
+                  "outline-none",
+                  "placeholder:text-textPrimary pr-10 placeholder:font-semibold focus:outline-none",
+                )}
+                ref={inputRef}
               />
+              <div className="absolute right-2 top-2 text-2xl font-bold ">
+                <HiOutlineSearch
+                  className="hover:text-primary"
+                  onClick={() => setIsSearching((prev) => !prev)}
+                />
+              </div>
             </div>
-          </div>
+            <span className="text-textPrimary text-2xl font-semibold">|</span>
+          </>
         )}
         <div className="rounded-full bg-primary p-3 text-xl font-semibold hover:opacity-80">
           <HiOutlineUser />
